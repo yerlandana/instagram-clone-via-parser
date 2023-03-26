2a52ebecb2fe8b10c1b17695cbe0c4df52c755d9
Dana <dyerlanova@gmail.com>
2023-03-26 03:58:02 -0500

    first-commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# instagram-clone-via-parser
- Codepath Lab 5
+# ios102-lab5-spring23
+
+![instaparse_walkthrough_1_xtra_small](https://user-images.githubusercontent.com/11927517/199710313-700aef85-ba89-427c-aa07-f89f0fdfdbbe.gif)
---
+++ lab-insta-parse/Feed/FeedViewController.swift
@@ -0,0 +1,82 @@
+//
+//  FeedViewController.swift
+//  lab-insta-parse
+//
+//  Created by Charlie Hieger on 11/1/22.
+//
+
+import UIKit
+
+// TODO: Import Parse Swift
+
+
+class FeedViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+
+    private var posts = [Post]() {
+        didSet {
+            // Reload table view data any time the posts variable gets updated.
+            tableView.reloadData()
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.allowsSelection = false
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        queryPosts()
+    }
+
+    private func queryPosts() {
+        // TODO: Pt 1 - Query Posts
+// https://github.com/parse-community/Parse-Swift/blob/3d4bb13acd7496a49b259e541928ad493219d363/ParseSwift.playground/Pages/2%20-%20Finding%20Objects.xcplaygroundpage/Contents.swift#L66
+
+
+    }
+
+    @IBAction func onLogOutTapped(_ sender: Any) {
+        showConfirmLogoutAlert()
+    }
+
+    private func showConfirmLogoutAlert() {
+        let alertController = UIAlertController(title: "Log out of your account?", message: nil, preferredStyle: .alert)
+        let logOutAction = UIAlertAction(title: "Log out", style: .destructive) { _ in
+            NotificationCenter.default.post(name: Notification.Name("logout"), object: nil)
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        alertController.addAction(logOutAction)
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true)
+    }
+
+    private func showAlert(description: String? = nil) {
+        let alertController = UIAlertController(title: "Oops...", message: "\(description ?? "Please try again...")", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default)
+        alertController.addAction(action)
+        present(alertController, animated: true)
+    }
+}
+
+extension FeedViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell", for: indexPath) as? PostCell else {
+            return UITableViewCell()
+        }
+        cell.configure(with: posts[indexPath.row])
+        return cell
+    }
+}
+
+extension FeedViewController: UITableViewDelegate { }
